03323765cf0448f3a6f3759943ab4c2bc93d8f65
Jose Cabrera <jose@codium.team>
2023-01-05 15:52:13 +0100

    Test: Should be able to search for a flights from Madrid to Barcelona

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,8 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Flights', () => {
-  it('It works', () => {
+  it('Should be able to search for a flights from Madrid to Barcelona', () => {
     cy.visit('https://qa-codium-course.netlify.app/');
-    cy.title().should('contains', 'Codium Flights.');
+    cy.get('#where-are-you-going-portal').click();
+    cy.get('#where-are-you-going #madrid').click();
+    cy.get('#to-where-are-you-going #barcelona').click();
+    cy.get('.relative:nth-child(4) > .relative > .relative').type('2023-01-06');
+    cy.get('.relative:nth-child(5) > .relative > .relative').type('2023-01-18');
+    cy.get('.mt-5').click();
+    cy.url().should('contains', 'https://qa-codium-course.netlify.app/search');
   });
 });
