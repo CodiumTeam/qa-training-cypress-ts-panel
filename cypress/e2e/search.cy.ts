8b27ce03e7fa9caf63c7591346964db5c3169dc4
Jose Cabrera <jose@codium.team>
2023-01-10 15:16:03 +0100

    Refactor: use accessibility selectors

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -3,36 +3,41 @@
 describe('Flights', () => {
   it('Should search flights from Madrid to Barcelona', () => {
     cy.visit('https://qa-codium-course.netlify.app/');
-    cy.get('#where-are-you-going-portal').click();
+
+    cy.findByLabelText('From').click();
     cy.get('#where-are-you-going #madrid').click();
     cy.get('#to-where-are-you-going #barcelona').click();
-    cy.get('.relative:nth-child(4) > .relative > .relative').type('2023-01-11');
-    cy.get('.relative:nth-child(5) > .relative > .relative').type('2023-01-18');
-    cy.get('.mt-5').click();
+    cy.findByPlaceholderText('Check in').type('2023-01-11');
+    cy.findByPlaceholderText('Check out').type('2023-01-18');
+    cy.findByRole('button', {name: 'Search'}).click();
+
     cy.url().should('contains', 'https://qa-codium-course.netlify.app/search');
   });
 
   it('Should show from - to dates in searched bar for a flight from Madrid to Barcelona', () => {
     cy.visit('https://qa-codium-course.netlify.app/');
-    cy.get('#where-are-you-going-portal').click();
+
+    cy.findByLabelText('From').click();
     cy.get('#where-are-you-going #madrid').click();
     cy.get('#to-where-are-you-going #barcelona').click();
-    cy.get('.relative:nth-child(4) > .relative > .relative').type('2023-01-11');
-    cy.get('.relative:nth-child(5) > .relative > .relative').type('2023-01-18');
-    cy.get('.mt-5').click();
-    cy.get('.icon-input:nth-child(1) > input').dblclick();
-    cy.get('.mt-4 > input').dblclick();
+    cy.findByPlaceholderText('Check in').type('2023-01-11');
+    cy.findByPlaceholderText('Check out').type('2023-01-18');
+    cy.findByRole('button', {name: 'Search'}).click();
+
+    cy.findByDisplayValue('11/01/2023').dblclick();
+    cy.findByDisplayValue('18/01/2023').dblclick();
   });
 
   it('Should list selectable routes flights prices for a flight from Madrid to Barcelona', () => {
     cy.visit('https://qa-codium-course.netlify.app/');
-    cy.get('#where-are-you-going-portal').click();
+
+    cy.findByLabelText('From').click();
     cy.get('#where-are-you-going #madrid').click();
     cy.get('#to-where-are-you-going #barcelona').click();
-    cy.get('.relative:nth-child(4) > .relative > .relative').type('2023-01-11');
-    cy.get('.relative:nth-child(5) > .relative > .relative').type('2023-01-18');
-    cy.get('.mt-5').click();
-    cy.get('.icon-input:nth-child(1) > input').dblclick();
+    cy.findByPlaceholderText('Check in').type('2023-01-11');
+    cy.findByPlaceholderText('Check out').type('2023-01-18');
+    cy.findByRole('button', {name: 'Search'}).click();
+
     cy.get('.flight-card:nth-child(1) .flight-card__price').dblclick();
     cy.get('.flight-card:nth-child(2) .flight-card__price').dblclick();
     cy.get('.flight-card:nth-child(3) .flight-card__price').dblclick();
