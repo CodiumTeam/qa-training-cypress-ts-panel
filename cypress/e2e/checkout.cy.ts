262eec261721ddd19ace09a8e5172072f62212e2
Jose Cabrera <jose@codium.team>
2023-01-11 11:27:31 +0100

    Refactor: simplify checkout tests with beforeEach

--- cypress/e2e/checkout.cy.ts
+++ cypress/e2e/checkout.cy.ts
@@ -1,9 +1,6 @@
 describe('Checkout', () => {
   beforeEach(() => {
     cy.visit('https://qa-codium-course.netlify.app/');
-  });
-
-  it.skip('Should be able to buy a flight from Madrid to Barcelona', () => {
     cy.findByLabelText('From').click();
     cy.selectFrom('#madrid');
     cy.selectTo('#barcelona');
@@ -14,6 +11,9 @@ describe('Checkout', () => {
     cy.findByPlaceholderText('Email Address').type(`info@codium.team`);
     cy.findByPlaceholderText('Password').type('codiumTest');
     cy.findByRole('button', {name: 'Login'}).click();
+  });
+
+  it.skip('Should be able to buy a flight from Madrid to Barcelona', () => {
     cy.findByLabelText('Card Holder').type('codium team');
     cy.findByLabelText('Card Details').type('4111111111111111');
     cy.findByPlaceholderText('MM/YY').type('03/25');
@@ -24,17 +24,6 @@ describe('Checkout', () => {
   });
 
   it('Should show flight summary in the checkout page', () => {
-    cy.findByLabelText('From').click();
-    cy.get('#where-are-you-going #madrid').click();
-    cy.get('#to-where-are-you-going #barcelona').click();
-    cy.findByPlaceholderText('Check in').type('2023-01-11');
-    cy.findByPlaceholderText('Check out').type('2023-01-18');
-    cy.findByRole('button', {name: 'Search'}).click();
-    cy.findAllByRole('button',{name: 'Select'}).first().click();
-    cy.findByPlaceholderText('Email Address').type(`info@codium.team`);
-    cy.findByPlaceholderText('Password').type('codiumTest');
-    cy.findByRole('button', {name: 'Login'}).click();
-
     cy.findByText(/flights from madrid to barcelona/i).should('be.visible');
     cy.findByText('93').should('be.visible');
   });
