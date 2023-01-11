0a6c0b0e99203ed54df21438b3242e581d3be50a
Jose Cabrera <jose@codium.team>
2023-01-11 09:59:44 +0100

    Refactor: use accessibility selectors in register tests

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -2,39 +2,42 @@ describe('Register page', () => {
   it('Should be able to register a new user coming from Home page', () => {
     const randomUserId = Date.now();
     cy.visit('https://qa-codium-course.netlify.app/');
-    cy.get('.rounded-lg').click();
-    cy.get('div:nth-child(1) > .px-5').click();
-    cy.get('div:nth-child(1) > .px-5').type('Cypress');
-    cy.get('div:nth-child(2) > .px-5').type('Test');
-    cy.get('div:nth-child(3) > .w-full').type(`new-random-${randomUserId}@codium.team`);
-    cy.get('div:nth-child(4) > .w-full').type('codiumTest');
-    cy.get('span').click();
-    cy.get('.grid').submit();
-    cy.get('.text-sm > a').click();
-    cy.get('.mb-4 .pl-2').click();
-    cy.get('.mb-4 .pl-2').type(`new-random-${randomUserId}@codium.team`);
-    cy.get('.flex:nth-child(4) .pl-2').type('codiumTest');
-    cy.get('.block').click();
-    cy.get('.bg-white:nth-child(1)').submit();
+    cy.findByRole('link', {
+      name: /Register/i
+    }).click();
+
+    cy.findByLabelText('First Name').type('Cypress');
+    cy.findByLabelText('Last name').type('Test');
+    cy.findByLabelText('Email address').type(`new-random-${randomUserId}@codium.team`);
+    cy.findByLabelText('Password').type(`codiumTest`);
+    cy.findByRole('button', {name: 'Sign Up'}).click();
+    cy.findByRole('link', {
+      name: 'Log in'
+    }).click();
+    cy.findByPlaceholderText('Email Address').type(`new-random-${randomUserId}@codium.team`);
+    cy.findByPlaceholderText('Password').type('codiumTest');
+    cy.findByRole('button', {name: 'Login'}).click();
+
     cy.url().should('contains', 'https://qa-codium-course.netlify.app/');
   });
 
   it('Should view "User already exists" message when tries to register with an existing user', () => {
     cy.visit('https://qa-codium-course.netlify.app/register');
-    cy.get('div:nth-child(1) > .px-5').dblclick();
-    cy.get('div:nth-child(1) > .px-5').type('Codium');
-    cy.get('div:nth-child(2) > .px-5').type('Team');
-    cy.get('div:nth-child(3) > .w-full').type('info@codium.team');
-    cy.get('div:nth-child(4) > .w-full').type('codiumTest');
-    cy.get('span').click();
-    cy.get('.grid').submit();
+
+    cy.findByLabelText('First Name').type('Codium');
+    cy.findByLabelText('Last name').type('Team');
+    cy.findByLabelText('Email address').type('info@codium.team');
+    cy.findByLabelText('Password').type(`codiumTest`);
+    cy.findByRole('button', {name: 'Sign Up'}).click();
+
     cy.get('.text-red-700').dblclick();
   });
 
   it('Should view "Required" message for all fields when user tries to register without filling fields', () => {
     cy.visit('https://qa-codium-course.netlify.app/register');
-    cy.get('.justify-between').click();
-    cy.get('.grid').submit();
+
+    cy.findByRole('button', {name: 'Sign Up'}).click();
+
     cy.get('div:nth-child(1) > .text-red-700').dblclick();
     cy.get('div:nth-child(2) > .text-red-700').dblclick();
     cy.get('div:nth-child(3) > .text-red-700').dblclick();
