d15dc834be4633a8eba194a15cf9a62e0718495d
Jose Cabrera <jose@codium.team>
2023-01-11 11:25:55 +0100

    Refactor: wrap some search test in describe and introduce new beforeEach for search process

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -16,29 +16,26 @@ describe('Flights', () => {
     cy.url().should('contains', 'https://qa-codium-course.netlify.app/search');
   });
 
-  it('Should show from - to dates in searched bar for a flight from Madrid to Barcelona', () => {
-    cy.findByLabelText('From').click();
-    cy.get('#where-are-you-going #madrid').click();
-    cy.get('#to-where-are-you-going #barcelona').click();
-    cy.findByPlaceholderText('Check in').type('2023-01-11');
-    cy.findByPlaceholderText('Check out').type('2023-01-18');
-    cy.findByRole('button', {name: 'Search'}).click();
+  describe('In search page', () => {
+    beforeEach(() => {
+      cy.findByLabelText('From').click();
+      cy.get('#where-are-you-going #madrid').click();
+      cy.get('#to-where-are-you-going #barcelona').click();
+      cy.findByPlaceholderText('Check in').type('2023-01-11');
+      cy.findByPlaceholderText('Check out').type('2023-01-18');
+      cy.findByRole('button', {name: 'Search'}).click();
+    });
 
-    cy.findByDisplayValue('11/01/2023').should('be.visible');
-    cy.findByDisplayValue('18/01/2023').should('be.visible');
-  });
+    it('Should show from - to dates in searched bar for a flight from Madrid to Barcelona', () => {
+      cy.findByDisplayValue('11/01/2023').should('be.visible');
+      cy.findByDisplayValue('18/01/2023').should('be.visible');
+    });
 
-  it('Should list selectable routes flights prices for a flight from Madrid to Barcelona', () => {
-    cy.findByLabelText('From').click();
-    cy.get('#where-are-you-going #madrid').click();
-    cy.get('#to-where-are-you-going #barcelona').click();
-    cy.findByPlaceholderText('Check in').type('2023-01-11');
-    cy.findByPlaceholderText('Check out').type('2023-01-18');
-    cy.findByRole('button', {name: 'Search'}).click();
-
-    cy.findByText('93').should('be.visible');
-    cy.findByText('68').should('be.visible');
-    cy.findByText('82').should('be.visible');
-    cy.findByText('91').should('be.visible');
+    it('Should list selectable routes flights prices for a flight from Madrid to Barcelona', () => {
+      cy.findByText('93').should('be.visible');
+      cy.findByText('68').should('be.visible');
+      cy.findByText('82').should('be.visible');
+      cy.findByText('91').should('be.visible');
+    });
   });
 });
