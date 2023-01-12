fed6e7aba83e27b4ebb46fb95a4d64ce5fcf644a
Jose Cabrera <jose@codium.team>
2023-01-12 12:23:20 +0100

    Install and configure cypress cucumber

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,23 @@
 import {defineConfig} from "cypress";
 import getCompareSnapshotsPlugin from "cypress-image-diff-js/plugin";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
       getCompareSnapshotsPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.feature',
   },
 });
