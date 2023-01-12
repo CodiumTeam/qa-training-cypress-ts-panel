89fb2e9d562b869479d5c215676e9b74de83720d
Jose Cabrera <jose@codium.team>
2023-01-12 11:13:11 +0100

    Should show "Invalid card holder" when the holder ame is not filled

--- cypress/e2e/checkout.cy.ts
+++ cypress/e2e/checkout.cy.ts
@@ -32,4 +32,26 @@ describe('Checkout', () => {
     cy.findByText(/flights from madrid to barcelona/i).should('be.visible');
     cy.findByText('93').should('be.visible');
   });
+
+  it('Should show "Invalid card holder" when the holder ame is not filled', () => {
+    cy.findByLabelText('Card Details').type('4111111111111111');
+    cy.findByPlaceholderText('MM/YY').type('03/25');
+    cy.window()
+      .then((win => {
+        cy.stub(win, 'prompt').returns('124');
+      }));
+    cy.findByPlaceholderText('CVC').click();
+    cy.window()
+      .then((win => {
+        cy.spy(win, 'alert').as('alertShown');
+      }));
+    cy.findByRole('button', {name: 'Place Order'})
+      .click();
+
+    cy.get("@alertShown")
+      .should(
+        "have.been.calledOnceWith",
+        "Invalid card because: Invalid Card holder"
+      );
+  });
 });
