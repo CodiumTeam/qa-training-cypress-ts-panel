5c0b11a9822120ca25b7231c0c6a776eb9801a89
sdaedd <edgoluc@gmail.com>
2024-05-27 10:32:34 +0200

    TestBusqueda

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -3,6 +3,17 @@
 describe('Flights', () => {
   it('It works', () => {
     cy.visit('https://qa-codium-course.netlify.app/');
-    cy.title().should('contains', 'Codium Flights.');
+    cy.get('#where-are-you-going-portal').click();
+    cy.get('#where-are-you-going #madrid').click();
+    cy.get('#to-where-are-you-going-portal').click();
+    cy.get('#to-where-are-you-going #barcelona').click();
+    cy.get('.relative:nth-child(4) > .relative > .relative').type('2024-05-16');
+    cy.get('.relative:nth-child(5) > .relative > .relative').type('2024-05-23');
+    cy.get('#adults-add path').click();
+    cy.get('.mt-5').click();
+    cy.url().should('contains', 'https://qa-codium-course.netlify.app/search');
+    cy.get('.mr-8').should('contain', 'BCN')
+    cy.get('.mr-8').should('contain', 'MAD')
+    cy.get('.mr-8').should('contain', 'MAD') 
   });
 });
---
+++ cypress/e2e/test.cy.ts
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('Flights', () => {
+  it('It works', () => {
+    cy.visit('https://qa-codium-course.netlify.app/');
+    cy.title().should('contains', 'Codium Flights.');
+    
+  });
+});
