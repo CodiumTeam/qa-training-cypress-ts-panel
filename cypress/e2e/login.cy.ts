7f70fd3594e63e0075ae9cec3762396bc9a94176
Jose Cabrera <jose@codium.team>
2024-05-27 13:02:42 +0200

    mi primer commit

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,10 +1,10 @@
 describe('Login', () => {
-  it('Should be able to log in from Home page', () => {
+  it.only('Should be able to log in from Home page', () => {
     cy.visit('https://qa-codium-course.netlify.app/');
     cy.get('.py-1').click();
     cy.get('.mb-4 > .flex').click();
     cy.get('.mb-4 .pl-2').click();
-    cy.get('.mb-4 .pl-2').type('codium@team.com');
+    cy.get('.mb-4 .pl-2').type('info@codium.team');
     cy.get('.flex:nth-child(4) .pl-2').type('codiumTest');
     cy.get('.block').click();
     cy.get('.bg-white:nth-child(1)').submit();
--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -1,18 +1,17 @@
 describe('Register page', () => {
   it('Should be able to register a new user coming from Home page', () => {
-    const randomUserId = Date.now();
     cy.visit('https://qa-codium-course.netlify.app/');
     cy.get('.rounded-lg').click();
     cy.get('div:nth-child(1) > .px-5').click();
     cy.get('div:nth-child(1) > .px-5').type('Cypress');
     cy.get('div:nth-child(2) > .px-5').type('Test');
-    cy.get('div:nth-child(3) > .w-full').type(`new-random-${randomUserId}@codium.team`);
+    cy.get('div:nth-child(3) > .w-full').type(`new-user@codium.team`);
     cy.get('div:nth-child(4) > .w-full').type('codiumTest');
     cy.get('span').click();
     cy.get('.grid').submit();
     cy.get('.text-sm > a').click();
     cy.get('.mb-4 .pl-2').click();
-    cy.get('.mb-4 .pl-2').type(`new-random-${randomUserId}@codium.team`);
+    cy.get('.mb-4 .pl-2').type(`new-user@codium.team`);
     cy.get('.flex:nth-child(4) .pl-2').type('codiumTest');
     cy.get('.block').click();
     cy.get('.bg-white:nth-child(1)').submit();
