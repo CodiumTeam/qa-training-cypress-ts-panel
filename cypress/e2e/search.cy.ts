6e84e3081f9dc4164a6985d6ff0de05ac2271c61
sdaedd <edgoluc@gmail.com>
2024-05-27 11:30:54 +0200

    TestBusqueda con comprobación de vuelo, fecha y pasajeros + TestLogin

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -9,6 +9,8 @@ let pasajeros = "2 adult"
 
 print
 describe('Flights', () => {
+  
+  // TestBusqueda: Testea la busqueda de vuelos con comprobación de vuelo, fecha y pasajeros
   it('Search Works OK', () => {
     cy.visit('https://qa-codium-course.netlify.app/');
     cy.get('#where-are-you-going-portal').click();
@@ -27,6 +29,7 @@ describe('Flights', () => {
     cy.get(".booking-bar__sub-heading").should('contain', pasajeros)
   });
 
+  // TestLogin: Testea el flujo de login y comprueba que esté logado.
   it('Login Works OK', () => {
     cy.visit('https://qa-codium-course.netlify.app/search?from=MAD&to=BCN&checkIn=16%2F05%2F2024&checkOut=23%2F05%2F2024&guests-adults=2&guests-children=0&guests-infants=0');
     cy.get('.py-1').click();
@@ -41,8 +44,7 @@ describe('Flights', () => {
   });
 });
 
-
-
+//Formatea la fecha de 2024-06-25 -> 25/06/2024
 function formatearFecha(fecha: string) {
   const [año, mes, dia] = fecha.split('-');
   return `${dia}/${mes}/${año}`
